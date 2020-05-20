3f64d68dfc3d008e6442f76850fb2ac8f8406c21
tania <tanislavens@yahoo.com>
2020-05-20 17:55:01 -0400

    added isEmpty problem

---
+++ javascript/veryEasy/isEmpty.js
@@ -0,0 +1,25 @@
+/*
+INSTRUCTIONS:
+Create a function that returns true if a string is empty and false otherwise.
+
+Examples
+isEmpty("") ➞ true
+
+isEmpty(" ") ➞ false
+
+isEmpty("a") ➞ false
+Notes
+A string containing only whitespaces " " does not count as empty.
+*/
+
+function isEmpty(s) {}
+
+/*
+TESTS:
+Test.assertEquals(isEmpty(""), true)
+Test.assertEquals(isEmpty(" "), false)
+Test.assertEquals(isEmpty("            "), false)
+Test.assertEquals(isEmpty("38215"), false)
+Test.assertEquals(isEmpty("afjabsdf"), false)
+Test.assertEquals(isEmpty("!?@&"), false)
+*/
