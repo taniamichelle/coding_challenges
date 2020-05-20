299e3c01120bf15c294f909af9e7342a18d6f177
tania <tanislavens@yahoo.com>
2020-05-20 17:46:42 -0400

    added concatenation problem

---
+++ javascript/veryEasy/concatName.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Given two strings, firstName and lastName, return a single string in the format "last, first".
+
+Examples
+concatName("First", "Last") ➞ "Last, First"
+
+concatName("John", "Doe") ➞ "Doe, John"
+
+concatName("Mary", "Jane") ➞ "Jane, Mary"
+*/
+
+function concatName(firstName, lastName) {}
+
+/*
+TESTS:
+Test.assertEquals(concatName("John", "Doe"), "Doe, John")
+Test.assertEquals(concatName("First", "Last"), "Last, First")
+Test.assertEquals(concatName("A", "B"), "B, A")
+
+// In case someone is making odd assumptions about comma characters.
+Test.assertEquals(concatName(",", ","), ",, ,")
+*/
