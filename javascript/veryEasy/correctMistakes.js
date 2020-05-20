edcb597178ddb790e6942e4aca99b5b529206961
tania <tanislavens@yahoo.com>
2020-05-20 17:51:01 -0400

    added correctMistakes file

---
+++ javascript/veryEasy/correctMistakes.js
@@ -0,0 +1,24 @@
+/*
+INSTRUCTIONS:
+Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+Examples
+squared(5) ➞ 25
+
+squared(9) ➞ 81
+
+squared(100) ➞ 10000
+*/
+
+function squaed(b) {
+  return $a * $a;
+}
+
+/*
+TESTS:
+Test.assertEquals(squared(10), 100, "Expected 100")
+Test.assertEquals(squared(69), 4761, "Expected 4761")
+Test.assertEquals(squared(666), 443556, "Expected 443556")
+Test.assertEquals(squared(-21), 441, "Expected 441")
+Test.assertEquals(squared(21), 441, "Expected 441")
+*/
