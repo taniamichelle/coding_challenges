a155c37585440748a4de86d14501d4fe5192ba79
tania <tanislavens@yahoo.com>
2020-05-21 18:06:37 -0400

    added lastElem file

---
+++ javascript/veryEasy/lastElem.js
@@ -0,0 +1,29 @@
+/*
+INSTRUCTIONS:
+Create a function that accepts an array and returns the last item in the array.
+
+Examples
+getLastItem([1, 2, 3]) ➞ 3
+
+getLastItem(["cat", "dog", "duck"]) ➞ "duck"
+
+getLastItem([true, false, true]) ➞ true
+*/
+
+function getLastItem(arr) {}
+
+/*
+TESTS:
+Test.assertEquals(getLastItem(['Cat', 'Dog', 'Duck']), 'Duck')
+Test.assertEquals(getLastItem([1, 2, 3]), 3)
+Test.assertEquals(getLastItem([undefined]))
+Test.assertEquals(getLastItem([true, false, false, true]), true)
+Test.assertEquals(getLastItem([7, 'String', false, undefined, null]), null)
+Test.assertEquals(getLastItem([false]), false)
+Test.assertEquals(getLastItem([undefined, undefined, undefined]), undefined)
+Test.assertEquals(getLastItem([1, 2, 3, 56, 87, 23, 65, 45]), 45)
+Test.assertEquals(getLastItem(['Apple', 'Orange', undefined]), undefined)
+Test.assertEquals(getLastItem([true, false, 'Apple']), 'Apple')
+Test.assertEquals(getLastItem([null, null, null]), null)
+Test.assertEquals(getLastItem([1]), 1)
+*/
