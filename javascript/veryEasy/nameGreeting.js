5c01f693b866a4e4ac9c1e87d15a459b63c5035c
tania <tanislavens@yahoo.com>
2020-05-22 17:30:27 -0400

    added nameGreeting file

---
+++ javascript/veryEasy/nameGreeting.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Create a function that takes a name and returns a greeting in the form of a string. Don't use a normal function but use an "arrow function".
+
+Examples
+helloName("Gerald") ➞ "Hello Gerald!"
+
+helloName("Tiffany") ➞ "Hello Tiffany!"
+
+helloName("Ed") ➞ "Hello Ed!"
+Notes
+The input is always a name (as string).
+*/
+
+const helloName = name => 
+
+/*
+TESTS:
+Test.assertEquals(helloName("Gerald"), "Hello Gerald!")
+Test.assertEquals(helloName("Fatima"), "Hello Fatima!")
+Test.assertEquals(helloName("Ed"), "Hello Ed!")
+Test.assertEquals(helloName("Tiffany"), "Hello Tiffany!")
+*/
