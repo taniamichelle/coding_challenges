3ba27f58d5081f952ae7b74a6e53ae158255277a
tania <tanislavens@yahoo.com>
2020-05-22 17:33:14 -0400

    added revArr file

---
+++ javascript/veryEasy/revArr.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Write a function to reverse an array.
+
+Examples
+reverse([1, 2, 3, 4]) ➞ [4, 3, 2, 1]
+
+reverse([9, 9, 2, 3, 4]) ➞ [4, 3, 2, 9, 9]
+
+reverse([]) ➞ []
+*/
+
+function reverse(arr) {}
+
+/*
+TESTS:
+Test.assertSimilar(reverse([1, 2, 3, 4]), [4, 3, 2, 1])
+Test.assertSimilar(reverse([5, 6, 7]), [7, 6, 5])
+Test.assertSimilar(reverse([9, 9, 2, 3, 4]), [4, 3, 2, 9, 9])
+Test.assertSimilar(reverse([3, 3]), [3, 3])
+Test.assertSimilar(reverse([-1, -1, -1]), [-1, -1, -1])
+Test.assertSimilar(reverse([]), [])
+*/
