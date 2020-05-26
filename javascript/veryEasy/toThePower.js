5b29d75f3812de92bfc3533bc66006d5edc7a4de
tania <tanislavens@yahoo.com>
2020-05-26 13:32:28 -0400

    added toThePower file

---
+++ javascript/veryEasy/toThePower.js
@@ -0,0 +1,22 @@
+/*
+INSTRUCTIONS:
+Create a function that takes a base number and an exponent number and returns the calculation.
+
+    Examples
+calculateExponent(5, 5) ➞ 3125
+
+calculateExponent(10, 10) ➞ 10000000000
+
+calculateExponent(3, 3) ➞ 27
+Notes
+All test inputs will be positive integers.
+*/
+
+function calculateExponent(num, exp) {}
+
+/*
+TESTS:
+Test.assertEquals(calculateExponent(5,5), 3125)
+Test.assertEquals(calculateExponent(3,3), 27)
+Test.assertEquals(calculateExponent(10,10), 10000000000)
+*/
