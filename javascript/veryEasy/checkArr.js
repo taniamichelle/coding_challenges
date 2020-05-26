e755831dc0a7da98e00f87d8445e60fb2da72456
tania <tanislavens@yahoo.com>
2020-05-26 13:42:05 -0400

    added checkArr file

---
+++ javascript/veryEasy/checkArr.js
@@ -0,0 +1,25 @@
+/*
+INSTRUCTIONS:
+Write a function to check if an array contains a particular number.
+
+    Examples
+check([1, 2, 3, 4, 5], 3) ➞ true
+
+check([1, 1, 2, 1, 1], 3) ➞ false
+
+check([5, 5, 5, 6], 5) ➞ true
+
+check([], 5) ➞ false
+*/
+
+function check(arr, el) {}
+
+/*
+TESTS:
+Test.assertEquals(check([1, 2, 3, 4, 5], 3), true)
+Test.assertEquals(check([1, 1, 2, 1, 1], 3), false)
+Test.assertEquals(check([1, 1, 2, 1, 5, 4, 7], 7), true)
+Test.assertEquals(check([1, 1, 2, 1, 5, 4, 7], 8), false)
+Test.assertEquals(check([5, 5, 5, 6], 5), true)
+Test.assertEquals(check([], 5), false)
+*/
