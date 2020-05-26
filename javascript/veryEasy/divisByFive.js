ee3133a304e37fff3b92fc9228c323f01bb0bb55
tania <tanislavens@yahoo.com>
2020-05-26 14:03:44 -0400

    added divisByFive file

---
+++ javascript/veryEasy/divisByFive.js
@@ -0,0 +1,24 @@
+/*
+INSTRUCTIONS:
+Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+
+Examples
+divisibleByFive(5) ➞ true
+
+divisibleByFive(-55) ➞ true
+
+divisibleByFive(37) ➞ false
+*/
+
+function divisibleByFive(n) {}
+
+/*
+TESTS:
+Test.assertEquals(divisibleByFive(7), false)
+Test.assertEquals(divisibleByFive(5), true)
+Test.assertEquals(divisibleByFive(15), true)
+Test.assertEquals(divisibleByFive(33), false)
+Test.assertEquals(divisibleByFive(-18), false)
+Test.assertEquals(divisibleByFive(999), false)
+Test.assertEquals(divisibleByFive(2), false)
+*/
