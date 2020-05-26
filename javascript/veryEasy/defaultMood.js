0ecd78fdf22218093645e10f543df6a886cbee43
tania <tanislavens@yahoo.com>
2020-05-26 13:55:00 -0400

    added defaultMood file

---
+++ javascript/veryEasy/defaultMood.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Create a function that takes in a current mood and return a sentence in the following format: "Today, I am feeling {mood}".However, if no argument is passed, return "Today, I am feeling neutral".
+
+    Examples
+moodToday("happy") ➞ "Today, I am feeling happy"
+
+moodToday("sad") ➞ "Today, I am feeling sad"
+
+moodToday() ➞ "Today, I am feeling neutral"
+*/
+
+function moodToday(mood) {}
+
+/*
+TESTS:
+Test.assertEquals(moodToday("happy"), "Today, I am feeling happy")
+Test.assertEquals(moodToday("sad"), "Today, I am feeling sad")
+Test.assertEquals(moodToday("very happy"), "Today, I am feeling very happy")
+Test.assertEquals(moodToday("rather empty inside"), "Today, I am feeling rather empty inside")
+Test.assertEquals(moodToday("confused"), "Today, I am feeling confused")
+Test.assertEquals(moodToday(), "Today, I am feeling neutral")
+*/
