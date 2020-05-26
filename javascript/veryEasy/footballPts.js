5a0dc199f3fe389e8a450921fe5e8c594fd7a2ce
tania <tanislavens@yahoo.com>
2020-05-26 13:49:48 -0400

    completed footballPts

--- javascript/veryEasy/footballPts.js
+++ javascript/veryEasy/footballPts.js
@@ -12,7 +12,9 @@ Notes
 Inputs will be numbers greater than or equal to 0.
 */
 
-function footballPoints(wins, draws, losses) {}
+function footballPoints(wins, draws, losses) {
+  return wins * 3 + draws * 1 + losses * 0;
+}
 
 /*
 TESTS:
