bd97658dba25cfc7ac6e2fef7a0ef2e6c48f0796
tania <tanislavens@yahoo.com>
2020-05-26 13:45:44 -0400

    added footballPts file

---
+++ javascript/veryEasy/footballPts.js
@@ -0,0 +1,24 @@
+/*
+INSTRUCTIONS:
+Create a function that takes the number of wins, draws and losses and calculates the number of points a football team has obtained so far. A win receives 3 points, a draw 1 point and a loss 0 points.
+
+Examples
+footballPoints(3, 4, 2) ➞ 13
+
+footballPoints(5, 0, 2) ➞ 15
+
+footballPoints(0, 0, 1) ➞ 0
+Notes
+Inputs will be numbers greater than or equal to 0.
+*/
+
+function footballPoints(wins, draws, losses) {}
+
+/*
+TESTS:
+Test.assertEquals(footballPoints(1, 2, 3), 5)
+Test.assertEquals(footballPoints(5, 5, 5), 20)
+Test.assertEquals(footballPoints(1, 0, 0), 3)
+Test.assertEquals(footballPoints(0, 7, 0), 7)
+Test.assertEquals(footballPoints(0, 0, 15), 0)
+*/
