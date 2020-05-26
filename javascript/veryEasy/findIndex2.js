f2e44e05b3338945448057d4db8927c4f4754a64
tania <tanislavens@yahoo.com>
2020-05-26 13:40:43 -0400

    completed findIndex2

--- javascript/veryEasy/findIndex2.js
+++ javascript/veryEasy/findIndex2.js
@@ -2,18 +2,21 @@
 INSTRUCTIONS:
 Create a function that searches for the index of a given item in an array.If the item is present, it should return the index, otherwise, it should return -1.
 
-Examples
+Examples:
 search([1, 2, 3, 4], 3) ➞ 2
 
 search([2, 4, 6, 8, 10], 8) ➞ 3
 
 search([1, 3, 5, 7, 9], 11) ➞ -1
-Notes
+
+Notes:
 If the item is not present, return -1.
 The given array is ordered.
 */
 
-function search(arr, item) {}
+function search(arr, item) {
+  return arr.indexOf(item);
+}
 
 /*
 TESTS:
