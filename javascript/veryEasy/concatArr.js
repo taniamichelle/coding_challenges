bdf6dd7f734c28d41705bf201193eae57503dcd7
tania <tanislavens@yahoo.com>
2020-05-26 13:37:18 -0400

    completed concatArr

--- javascript/veryEasy/concatArr.js
+++ javascript/veryEasy/concatArr.js
@@ -10,7 +10,9 @@ concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
 concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
 */
 
-function concat(arr1, arr2) {}
+function concat(arr1, arr2) {
+  return arr1.concat(arr2);
+}
 
 /*
 TESTS:
