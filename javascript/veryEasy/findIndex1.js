bcc7313636987cfed7819b929ca6e6ed7b555da3
tania <tanislavens@yahoo.com>
2020-05-28 18:26:59 -0400

    added findIndex1 file

---
+++ javascript/veryEasy/findIndex1.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Create a function that finds the index of a given item.
+
+Examples
+search([1, 5, 3], 5) ➞ 1
+
+search([9, 8, 3], 3) ➞ 2
+
+search([1, 2, 3], 4) ➞ -1
+Notes
+If the item is not present, return -1.
+*/
+
+function search(arr, item) {}
+
+/*
+TESTS:
+Test.assertEquals(search([1, 5, 3], 5), 1)
+Test.assertEquals(search([9, 8, 3], 3), 2)
+Test.assertEquals(search([1, 2, 3], 4), -1)
+Test.assertEquals(search([1, 5, 3, 10, 17, 20, -6], 20), 5)
+*/
