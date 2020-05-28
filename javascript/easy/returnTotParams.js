c998639323bbf67dde68f0f0675d6dd1ee62e8eb
tania <tanislavens@yahoo.com>
2020-05-28 18:04:40 -0400

    added returnTotParmas file

---
+++ javascript/easy/returnTotParams.js
@@ -0,0 +1,26 @@
+/*
+INSTRUCTIONS:
+Create a function that returns the total number of parameters passed in.
+
+Examples:
+numberArgs("a", "b", "c") ➞ 3
+
+numberArgs(10, 20, 30, 40, 50) ➞ 5
+
+numberArgs(x, y) ➞ 2
+
+numberArgs() ➞ 0
+
+Notes:
+How can you express the input parameter so it takes a variable number of arguments?
+*/
+
+function numberArgs(/* fill-in */) {}
+
+/*
+TESTS:
+Test.assertEquals(numberArgs('a', 'b', 'c'), 3)
+Test.assertEquals(numberArgs(10, 20, 30, 40, 50), 5)
+Test.assertEquals(numberArgs('x', 'y'), 2)
+Test.assertEquals(numberArgs(), 0)
+*/
