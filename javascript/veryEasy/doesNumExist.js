1605847d4499faddc4fb2c68bbc4c924fa28d340
tania <tanislavens@yahoo.com>
2020-06-01 16:32:28 -0400

    added doesNumExist file

---
+++ javascript/veryEasy/doesNumExist.js
@@ -0,0 +1,33 @@
+/*
+INSTRUCTIONS:
+Create a function which validates whether a given number exists, and could represent a real life quantity. Inputs will be given as a string.
+
+Examples:
+validStrNumber("3.2") ➞ true
+
+validStrNumber("324") ➞ true
+
+validStrNumber("54..4") ➞ false
+
+validStrNumber("number") ➞ false
+
+Notes:
+Accept numbers such as .5 and 0003.
+*/
+
+function validStrNumber(n) {}
+
+/*
+TESTS:
+Test.assertEquals(validStrNumber("3.2"), true)
+Test.assertEquals(validStrNumber("324"), true)
+Test.assertEquals(validStrNumber("54..4"), false)
+Test.assertEquals(validStrNumber("number"), false)
+Test.assertEquals(validStrNumber(".5"), true)
+Test.assertEquals(validStrNumber("03"), true)
+Test.assertEquals(validStrNumber("3.e"), false)
+Test.assertEquals(validStrNumber("1234321"), true)
+Test.assertEquals(validStrNumber(".42.3"), false)
+Test.assertEquals(validStrNumber("0000000"), true)
+Test.assertEquals(validStrNumber("000.000"), true)
+*/
