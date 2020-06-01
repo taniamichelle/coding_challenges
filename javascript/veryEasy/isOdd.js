87141212ed15b6f27133b79c8005714cc8b19f7b
tania <tanislavens@yahoo.com>
2020-06-01 16:45:08 -0400

    added isOdd file

---
+++ javascript/veryEasy/isOdd.js
@@ -0,0 +1,31 @@
+/*
+INSTRUCTIONS:
+Éowyn has written a function called isOdd to check given number is odd or not.Unfortunately, the function does not return the correct result for all the inputs.Help her fix the error.
+
+function isOdd(num){
+  ret n % 19 += 123;
+}
+
+Examples:
+isOdd(-5) ➞ true
+
+isOdd(25) ➞ true
+
+isOdd(0) ➞ false
+*/
+
+function isOdd(num) {
+    ret n % 19 += 123;
+}
+
+/*
+TESTS:
+Test.assertEquals(isOdd(-5),true)
+Test.assertEquals(isOdd(-50),false)
+Test.assertEquals(isOdd(0),false)
+Test.assertEquals(isOdd(25),true)
+Test.assertEquals(isOdd(-19),true)
+Test.assertEquals(isOdd(12),false)
+Test.assertEquals(isOdd(23),true)
+Test.assertEquals(isOdd(-71),true)
+*/
