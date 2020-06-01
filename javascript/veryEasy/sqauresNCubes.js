cd372fb3051e54742141a0f73a7d8c9dd189bb87
tania <tanislavens@yahoo.com>
2020-06-01 18:58:32 -0400

    added squaresNCubes file

---
+++ javascript/veryEasy/sqauresNCubes.js
@@ -0,0 +1,30 @@
+/*
+INSTRUCTIONS:
+Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+
+Examples
+checkSquareAndCube([4, 8]) ➞ true
+
+checkSquareAndCube([16, 48]) ➞ false
+
+checkSquareAndCube([9, 27]) ➞ true
+Notes
+Remember to return either true or false.
+All arrays contain two positive numbers.
+*/
+
+function checkSquareAndCube(arr) {}
+
+/*
+TESTS:
+Test.assertEquals(checkSquareAndCube([4, 8]), true)
+Test.assertEquals(checkSquareAndCube([5, 12]), false)
+Test.assertEquals(checkSquareAndCube([9, 27]), true)
+Test.assertEquals(checkSquareAndCube([25, 120]), false)
+Test.assertEquals(checkSquareAndCube([25, 125]), true)
+Test.assertEquals(checkSquareAndCube([36, 215]), false)
+Test.assertEquals(checkSquareAndCube([36, 217]), false)
+Test.assertEquals(checkSquareAndCube([144, 1728]), true)
+Test.assertEquals(checkSquareAndCube([1, 1]), true)
+Test.assertEquals(checkSquareAndCube([676, 17576]), true)
+*/
