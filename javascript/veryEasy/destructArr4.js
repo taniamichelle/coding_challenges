9ced72186411a2641ca2f2642960572a951efb16
tania <tanislavens@yahoo.com>
2020-06-01 18:17:48 -0400

    completed destructArr4 file

--- javascript/veryEasy/destructArr4.js
+++ javascript/veryEasy/destructArr4.js
@@ -11,7 +11,8 @@ But how could I make tail = [2, 3, 4] instead of tail = 2?
 
 // Change the string so that it will evaluate to head = 1 and tail = [2, 3, 4]
 // Use the Rest element
-const str = "[head, tail] = [1, 2, 3, 4]";
+// const str = "[head, tail] = [1, 2, 3, 4]";
+const str = "[head, ...tail] = [1, 2, 3, 4]";
 
 /*
 TESTS:
