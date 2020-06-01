15017a3f1ba60bd2bed6c4aee2e3a479939f289d
tania <tanislavens@yahoo.com>
2020-06-01 18:20:33 -0400

    added lastChar file

---
+++ javascript/veryEasy/lastChar.js
@@ -0,0 +1,26 @@
+/* 
+INSTRUCTIONS:
+Create a function that takes a string (a random name). If the last character of the name is an "n", return true, otherwise return false.
+
+Examples
+isLastCharacterN("Aiden") ➞ true
+
+isLastCharacterN("Piet") ➞ false
+
+isLastCharacterN("Bert") ➞ false
+
+isLastCharacterN("Dean") ➞ true
+*/
+
+function isLastCharacterN(word) {}
+
+/* 
+TESTS:
+Test.assertEquals(isLastCharacterN("Aiden"), true)
+Test.assertEquals(isLastCharacterN("Roxy"), false)
+Test.assertEquals(isLastCharacterN("Bert"), false)
+Test.assertEquals(isLastCharacterN("Dean"), true)
+Test.assertEquals(isLastCharacterN("Ian"), true)
+Test.assertEquals(isLastCharacterN("Brian"), true)
+Test.assertEquals(isLastCharacterN("Daniel"), false)
+*/
