fdfa34d5d7414475c4ec47b7055d2f946ea0442d
tania <tanislavens@yahoo.com>
2020-06-02 16:28:18 -0400

    added filterArr file

---
+++ javascript/veryEasy/filterArr.js
@@ -0,0 +1,21 @@
+/*
+INSTRUCTIONS:
+Create a function that takes an array of strings and numbers, and filters out the array so that it returns an array of integers only.
+
+Examples:
+filterArray([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
+
+filterArray(["A", 0, "Edabit", 1729, "Python", "1729"]) ➞ [0, 1729]
+
+filterArray(["Nothing", "here"]) ➞ []
+*/
+
+function filterArray(arr) {}
+
+/*
+TESTS:
+Test.assertSimilar(filterArray([1, 2, 3, "a", "b", 4]), [1, 2, 3, 4])
+Test.assertSimilar(filterArray(["A", 1, "&amp", 0, -9, "Edabit"]), [1, 0, -9])
+Test.assertSimilar(filterArray(["Nothing", "here"]),[])
+Test.assertSimilar(filterArray([1, 2, 3, 3.5 , "a", "b", 4 , 5.8 , 6]), [1, 2, 3, 4 , 6])
+*/
