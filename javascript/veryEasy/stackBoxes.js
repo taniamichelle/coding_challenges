aa91a3648887906a7b07ba125c5fdaee7602a995
tania <tanislavens@yahoo.com>
2020-06-02 18:32:12 -0400

    completed stackBoxes

--- javascript/veryEasy/stackBoxes.js
+++ javascript/veryEasy/stackBoxes.js
@@ -12,3 +12,19 @@ stackBoxes(2) ➞ 4
 stackBoxes(0) ➞ 0
 Notes
 Step n is a positive integer.
+*/
+
+function stackBoxes(n) {
+  return Math.pow(n, 2);
+}
+
+/*
+TESTS:
+Test.assertEquals(stackBoxes(1), 1)
+Test.assertEquals(stackBoxes(2), 4)
+Test.assertEquals(stackBoxes(0), 0)
+Test.assertEquals(stackBoxes(5), 25)
+Test.assertEquals(stackBoxes(27), 729)
+Test.assertEquals(stackBoxes(196), 38416)
+Test.assertEquals(stackBoxes(512), 262144)
+*/
