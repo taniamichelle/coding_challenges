1c0995bc0c1f10b3151f154464f7cc9e508fa7db
tania <tanislavens@yahoo.com>
2020-06-02 15:22:38 -0400

    added countArgs file

---
+++ javascript/veryEasy/countArgs.js
@@ -0,0 +1,29 @@
+/*
+INSTRUCTIONS:
+Create a function that returns the number of arguments it was called with.
+
+Examples
+numArgs() ➞ 0
+
+numArgs("foo") ➞ 1
+
+numArgs("foo", "bar") ➞ 2
+
+numArgs(true, false) ➞ 2
+
+numArgs({}) ➞ 1
+*/
+
+/*
+TESTS:
+Test.assertEquals(numArgs(), 0)
+Test.assertEquals(numArgs('foo'), 1)
+Test.assertEquals(numArgs('foo', 'bar'), 2)
+Test.assertEquals(numArgs(null, null), 2)
+Test.assertEquals(numArgs('foo', 'bar', 5, null), 4)
+Test.assertEquals(numArgs(false), 1)
+Test.assertEquals(numArgs('foo', {}), 2)
+Test.assertEquals(numArgs('foo', 'bar', {}), 3)
+Test.assertEquals(numArgs([], [], {}), 3)
+Test.assertEquals(numArgs('foo', 'bar', true, null, 0), 5)
+*/
