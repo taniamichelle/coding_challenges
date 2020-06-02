20e1e2cafa72951695c60faf926a948250992bc5
tania <tanislavens@yahoo.com>
2020-06-02 16:36:32 -0400

    added returnStr file

---
+++ javascript/veryEasy/returnStr.js
@@ -0,0 +1,27 @@
+/*
+INSTRUCTIONS:
+Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+
+Examples:
+longBurp(3) ➞ "Burrrp"
+
+longBurp(5) ➞ "Burrrrrp"
+
+longBurp(9) ➞ "Burrrrrrrrrp"
+
+Notes:
+Expect num to always be >= 1.
+*/
+
+function longBurp(num) {}
+
+/*
+TESTS:
+Test.assertEquals(longBurp(3), "Burrrp")
+Test.assertEquals(longBurp(5), "Burrrrrp")
+Test.assertEquals(longBurp(9), "Burrrrrrrrrp")
+Test.assertEquals(longBurp(10), "Burrrrrrrrrrp")
+Test.assertEquals(longBurp(13), "Burrrrrrrrrrrrrp")
+Test.assertEquals(longBurp(18), "Burrrrrrrrrrrrrrrrrrp")
+Test.assertEquals(longBurp(1), "Burp")
+*/
