bb570329fb520a460a1cf489d748d67113bea3b2
tania <tanislavens@yahoo.com>
2020-06-03 16:35:25 -0400

    added boxVol file

---
+++ javascript/veryEasy/boxVol.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Create a function that takes an object argument sizes (contains width, length, height keys) and returns the volume of the box.
+
+Examples:
+volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
+
+volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
+
+volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+*/
+
+function volumeOfBox(sizes) {}
+
+/*
+TESTS:
+Test.assertEquals(volumeOfBox({width: 11,length: 1, height: 2}), 22)
+Test.assertEquals(volumeOfBox({width: 1, length: 5,  height: 3}), 15)
+Test.assertEquals(volumeOfBox({width: 2, length: 1,  height: 1}), 2)
+Test.assertEquals(volumeOfBox({width: 2, length: 6,  height: 5}), 60)
+Test.assertEquals(volumeOfBox({width: 2, length: 5,  height: 3}), 30)
+Test.assertEquals(volumeOfBox({width: 2, length: 5,  height: 1}), 10)
+*/
