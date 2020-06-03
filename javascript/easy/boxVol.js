a2bd369e6f79106529249e47606f70701036ac62
tania <tanislavens@yahoo.com>
2020-06-03 17:25:25 -0400

    completed boxVol

--- javascript/easy/boxVol.js
+++ javascript/easy/boxVol.js
@@ -8,9 +8,12 @@ volumeOfBox({ width: 2, length: 5, height: 1 }) ➞ 10
 volumeOfBox({ width: 4, length: 2, height: 2 }) ➞ 16
 
 volumeOfBox({ width: 2, length: 3, height: 5 }) ➞ 30
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
 */
 
-function volumeOfBox(sizes) {}
+function volumeOfBox(sizes) {
+  return sizes.width * sizes.length * sizes.height;
+}
 
 /*
 TESTS:
