2b46aaaf48b46305d844b3aa6deb9a4401c73c61
tania <tanislavens@yahoo.com>
2020-06-04 18:15:07 -0400

    added flipBool file

---
+++ javascript/veryEasy/flipBool.js
@@ -0,0 +1,23 @@
+/*
+INSTRUCTIONS:
+Booleans can also be written as integers, where 1 = True and 0 = False. Make a function that returns the opposite of the boolean given.
+
+Examples
+flipBool(true) ➞ 0
+
+flipBool(false) ➞ 1
+
+flipBool(1) ➞ 0
+
+flipBool(0) ➞ 1
+*/
+
+function flipBool(b) {}
+
+/*
+TESTS:
+Test.assertEquals(flipBool(1), 0)
+Test.assertEquals(flipBool(true), 0)
+Test.assertEquals(flipBool(0), 1)
+Test.assertEquals(flipBool(false), 1)
+*/
