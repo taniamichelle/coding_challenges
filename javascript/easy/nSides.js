69624e8ddc8dbb1d65e414c93facb58dbfb22b06
tania <tanislavens@yahoo.com>
2020-06-09 12:24:57 -0400

    completed nSides

--- javascript/easy/nSides.js
+++ javascript/easy/nSides.js
@@ -25,6 +25,41 @@ Return the output in lowercase.
 The challenge is intended to be completed without conditionals (it would take too long)!
 */
 
+function nSidedShape(n) {
+  switch (n) {
+    case 1:
+      return "circle";
+      break;
+    case 2:
+      return "semi-circle";
+      break;
+    case 3:
+      return "triangle";
+      break;
+    case 4:
+      return "square";
+      break;
+    case 5:
+      return "pentagon";
+      break;
+    case 6:
+      return "hexagon";
+      break;
+    case 7:
+      return "heptagon";
+      break;
+    case 8:
+      return "octagon";
+      break;
+    case 9:
+      return "nonagon";
+      break;
+    case 10:
+      return "decagon";
+      break;
+  }
+}
+
 /*
 TESTS:
 Test.assertEquals(nSidedShape(1), "circle")
