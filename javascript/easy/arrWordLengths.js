517a963958ebadf76b4fb223b975c177e8f76317
tania <tanislavens@yahoo.com>
2020-06-09 15:40:19 -0400

    added arrWordLengths

---
+++ javascript/easy/arrWordLengths.js
@@ -0,0 +1,25 @@
+/*
+INSTRUCTIONS:
+Create a function that takes an array of words and transforms it into an array of each word's length.
+
+Examples
+wordLengths(["hello", "world"]) ➞ [5, 5]
+
+wordLengths(["Halloween", "Thanksgiving", "Christmas"]) ➞ [9, 12, 9]
+
+wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]) ➞ [3, 5, 9, 4, 2, 3, 8]
+Notes
+No test case will contain punctuation.
+Arrays can be of various lengths.
+*/
+
+function wordLengths(arr) {}
+
+/*
+TESTS:
+Test.assertSimilar(wordLengths(["hello", "world"]), [5, 5], "Example #1")
+Test.assertSimilar(wordLengths(["Halloween", "Thanksgiving", "Christmas"]), [9, 12, 9], "Example #2")
+Test.assertSimilar(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"]), [3, 5, 9, 4, 2, 3, 8], "Example #3")
+Test.assertSimilar(wordLengths(["Indiana", "Jones", "and", "the", "Temple", "of", "Doom"]), [7, 5, 3, 3, 6, 2, 4])
+Test.assertSimilar(wordLengths(["Programming", "is", "fun"]), [11, 2, 3])
+*/
