35329977ed305cdf0d9d8772712859e1754a5caa
tania <tanislavens@yahoo.com>
2020-06-09 14:55:45 -0400

    completed destructArr2

--- javascript/veryEasy/destructArr2.js
+++ javascript/veryEasy/destructArr2.js
@@ -20,7 +20,7 @@ const arr = ["cars", "planes", ["trains", ["motorcycles"]]];
 
 // Fix the following using es6 destructuring
 // Only edit what's inside of [ trans1, trans2, trans3, trans4 ]
-let [trans1, trans2, trans3, trans4] = arr;
+let [trans1, trans2, [trans3, [trans4]]] = arr;
 
 /*
 console.log(trans1) // should output "cars"
