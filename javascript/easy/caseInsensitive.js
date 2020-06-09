4ffa36fbacc3f558ce657ff2d5854efd1bdea708
tania <tanislavens@yahoo.com>
2020-06-09 14:57:43 -0400

    added caseInsensitive file

---
+++ javascript/easy/caseInsensitive.js
@@ -0,0 +1,30 @@
+/*
+INSTRUCTIONS:
+Write a function that validates whether two strings are identical. Make it case insensitive.
+
+Examples
+match("hello", "hELLo") ➞ true
+
+match("motive", "emotive") ➞ false
+
+match("venom", "VENOM") ➞ true
+
+match("mask", "mAskinG") ➞ false
+*/
+
+function match(s1, s2) {}
+
+/*
+TESTS:
+Test.assertEquals(match("hello", "hELLo"), true)
+Test.assertEquals(match("hey", "hey"), true)
+Test.assertEquals(match("venom", "VENOM"), true)
+Test.assertEquals(match("maGIciAN", "magician"), true)
+Test.assertEquals(match("stupIFy", "stupifY"), true)
+
+Test.assertEquals(match("bald", "blad"), false)
+Test.assertEquals(match("motive", "emotive"), false)
+Test.assertEquals(match("mask", "mAskinG"), false)
+Test.assertEquals(match("skim", "skimp"), false)
+Test.assertEquals(match("EXCEl", "exceLs"), false)
+*/
