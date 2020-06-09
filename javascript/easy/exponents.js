1005235fd2c9f3b8481f78ab2632d32aa5060e03
tania <tanislavens@yahoo.com>
2020-06-09 15:01:43 -0400

    added exponents file

---
+++ javascript/easy/exponents.js
@@ -0,0 +1,29 @@
+/*
+INSTRUCTIONS:
+Write a function that returns true if k^k == n for input (n, k) and return false otherwise.
+
+Examples
+kToK(4, 2) ➞ true
+
+kToK(387420489, 9) ➞ true
+// 9^9 == 387420489
+
+kToK(3124, 5) ➞ false
+
+kToK(17, 3) ➞ false
+Notes
+The ^ operator refers to exponentiation operation, not the bitwise XOR operation.
+*/
+
+function kToK(n, k) {}
+
+/*
+TESTS:
+Test.assertEquals(kToK(4, 2), true)
+Test.assertEquals(kToK(387420489, 9), true)
+Test.assertEquals(kToK(302875106592253, 13), true)
+
+Test.assertEquals(kToK(3124, 5), false)
+Test.assertEquals(kToK(17, 3), false)
+Test.assertEquals(kToK(823544, 7), false)
+*/
