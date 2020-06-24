38bac9576ab0702ff29b3bd22bb16fe18ea36169
tania <tanislavens@yahoo.com>
2020-06-24 18:14:20 -0400

    added charToASCII file

---
+++ javascript/easy/charToASCII.js
@@ -0,0 +1,25 @@
+/*
+INSTRUCTIONS:
+Create a function that returns the ASCII value of the passed in character.
+
+Examples
+ctoa("A") ➞ 65
+
+ctoa("m") ➞ 109
+
+ctoa("[") ➞ 91
+
+ctoa("\") ➞ 92
+
+*/
+
+function ctoa(c) {}
+
+/*
+TESTS:
+Test.assertEquals(ctoa(' '), 32)
+Test.assertEquals(ctoa('A'), 65)
+Test.assertEquals(ctoa(']'), 93)
+Test.assertEquals(ctoa('^'), 94)
+Test.assertEquals(ctoa('c'), 99)
+*/
