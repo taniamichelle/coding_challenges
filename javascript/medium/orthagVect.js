77ac6064782d7242aa5a7e9574def23d381c3941
tania <tanislavens@yahoo.com>
2020-07-08 17:44:54 -0400

    added orthagVect file

---
+++ javascript/medium/orthagVect.js
@@ -0,0 +1,25 @@
+/*
+INSTRUCTIONS:
+Create a function that takes two vectors as arrays and checks if the two vectors are orthogonal or not. The return value is boolean. Two vectors a and b are orthogonal if their dot product is equal to zero.
+
+Examples
+isOrthogonal([1, 2], [2, -1]) ➞ true
+
+isOrthogonal([3, -1], [7, 5]) ➞ false
+
+isOrthogonal([1, 2, 0], [2, -1, 10]) ➞ true
+Notes
+The two arrays will be of same length.
+*/
+
+function isOrthogonal(arr1, arr2) {}
+
+/*
+TESTS:
+Test.assertEquals(isOrthogonal([7, 8], [7, -6]), false)
+Test.assertEquals(isOrthogonal([-13, -26], [-8, 4]), true)
+Test.assertEquals(isOrthogonal([1, 2], [2, -1]), true)
+Test.assertEquals(isOrthogonal([2, 4, 1], [2, 1, -8]), true)
+Test.assertEquals(isOrthogonal([1, 2, 0], [2, -1, 10]), true)
+Test.assertEquals(isOrthogonal([3, 8, 9], [16, 17, -18]), false)
+*/
